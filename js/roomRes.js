85aaebe9e4c71b06864e4914f3b8c85c833647a0
annyhynna <annyhsu23@gmail.com>
2017-01-21 16:50:23 -0800

    Add firebase js file

---
+++ js/roomRes.js
@@ -0,0 +1,8 @@
+// javascript for firebase database
+
+// Get a reference to the database service
+var database = firebase.database();
+
+console.log(firebase);
+
+var roomName = firebase.database().ref('');
